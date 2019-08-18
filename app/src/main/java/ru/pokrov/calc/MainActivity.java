0b8e04f673407c72d01c9badb40229d9b84d10c8
pokrov <al.n.pokrovsky@gmail.com>
2019-08-18 22:37:34 +0300

    init

---
+++ app/src/main/java/ru/pokrov/calc/MainActivity.java
@@ -0,0 +1,26 @@
+package ru.pokrov.calc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvResult = findViewById(R.id.tvResult);
+    }
+
+    public void onDigitClick(View view) {
+        CharSequence btn = ((Button)view).getText();
+        tvResult.append(btn);
+    }
+}
